3a7d4aaf5d34ae1e0deddf1f65c2635f1bd970d7
Jw <jwhaeke@live.nl>
2018-12-12 14:39:05 +0100

    Starting an oop address book

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,30 @@
-class Person {
-    constructor(id, firstName, lastName, gitHub) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gitHub = gitHub;
-    }
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
+// class Person {
+//     constructor(id, firstName, lastName, gitHub) {
+//         this.id = id;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.gitHub = gitHub;
+//     }
+//     getFullName() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
 
-const jw = new Person('jhaeke', 'Jan-Willem', 'Haeke', 'www.google.come');
+// const jw = new Person('jhaeke', 'Jan-Willem', 'Haeke', 'www.google.come');
 
-console.log(jw.getFullName());
+// console.log(jw.getFullName());
+
+function create(){
+        // create a new div element 
+        var newDiv = document.createElement("div"); 
+        // and give it some content 
+        var newContent = document.createTextNode("Hi there and greetings!"); 
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);  
+      
+        // add the newly created element and its content into the DOM 
+        var currentDiv = document.getElementById("div1"); 
+        document.body.insertBefore(newDiv, currentDiv);
+        
+         
+      }
