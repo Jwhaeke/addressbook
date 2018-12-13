337c0737e34d1b701386cd1f804d5e91fe3ab6e6
jwhaeke <jwhaeke@live.nl>
2018-12-13 14:32:21 +0100

    Added a sort function and filter list. Also added an add function

--- javascript/main.js
+++ javascript/main.js
@@ -28,6 +28,10 @@ class Person {
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
     }
+    
+
+
+    
     plot() {
         var newDiv = document.createElement("div"); 
         newDiv.setAttribute("class", "callingCard" );
@@ -40,6 +44,8 @@ class Person {
     }
 
 }
+
+
 
 // var book {
     
@@ -89,12 +95,149 @@ var list = [
 
 function create() {
     //p1.plot();
-list.forEach(element => {
-    element.plot();
-  });
+// list.forEach(element => {
+//     element.plot();
+//   });
+    list.forEach(element => {
+        var named = element.getFullName();
+        document.write(named);
+    })
+
 }
 
+function write() {
+        
+    var ul = document.createElement('ul');
+    ul.setAttribute("id", "id1" );
+    document.getElementById('div1').appendChild(ul);
+    
+    list.forEach(element =>  {
+        var named = element.getFullName();
+        var li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML += named;
+    });
+}
+//Sort script from W3Schools --- Looking into the process
+function sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("id1");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // start by saying: no switching is done:
+        switching = false;
+        b = list.getElementsByTagName("LI");
+        // Loop through all list-items:
+        for (i = 0; i < (b.length - 1); i++) {
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+        /* check if the next item should
+        switch place with the current item: */
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+            /* if next item is alphabetically
+            lower than current item, mark as a switch
+            and break the loop: */
+            shouldSwitch = true;
+            break;
+        }
+        }
+        if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+        }
+    }   
+}
 
+function sortList2() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("id1");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // start by saying: no switching is done:
+        switching = false;
+        b = list.getElementsByTagName("LI");
+        // Loop through all list-items:
+        for (i = 0; i < (b.length - 1); i++) {
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+        /* check if the next item should
+        switch place with the current item: */
+        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+            /* if next item is alphabetically
+            lower than current item, mark as a switch
+            and break the loop: */
+            shouldSwitch = true;
+            break;
+        }
+        }
+        if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+        }
+    }   
+}
+
+function voegIemandToe(){
+    var node = document.createElement("LI");
+    var sNaam = document.getElementById("myNewName").value
+    var textnode = document.createTextNode(sNaam);
+    node.appendChild(textnode);
+    document.getElementById("id1").appendChild(node);
+}
+
+function myFilterFunction() {
+
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myFilterBox');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("id1");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+    //a = li[i].getElementsByTagName("a")[0]; Not needed - no <a> present (Can be deleted)
+    txtValue = li[i].innerHTML;
+      
+// Loop through all list items, and hide those who don't match the search query
+
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  
+}
+}
+
+function myHighLight() {
+
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('highlighter');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("id1");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+    //a = li[i].getElementsByTagName("a")[0]; Not needed - no <a> present (Can be deleted)
+    txtValue = li[i].innerHTML;
+      
+// Loop through all list items, and hide those who don't match the search query
+
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.color = "tomato";
+    } else {
+      li[i].style.color = "black";
+    }
+  
+}
+}
 
 
 
